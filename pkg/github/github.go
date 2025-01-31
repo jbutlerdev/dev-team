72e19b43715195925a05dd10a4e8de8c2cdc6756
GitWatcher <gitwatcher@local>
2025-01-30 22:18:27 -0500

    Fix github client timeout issue

--- pkg/github/github.go
+++ pkg/github/github.go
@@ -100,6 +100,62 @@ func CreateDraftPR(path string, githubToken string, input GitHubPRInput) error {
 	return nil
 }
 
+func CreatePR(path string, githubToken string, input GitHubPRInput, labels []string) error {
+	ctx := context.Background()
+	client := newGitHubClient(ctx, githubToken)
+
+	repo, err := git.PlainOpen(path)
+	if err != nil {
+		return err
+	}
+
+	remote, err := repo.Remote("origin")
+	if err != nil {
+		return fmt.Errorf("error getting remote: %v", err)
+	}
+
+	remoteURL := remote.Config().URLs[0]
+	var owner, repoName string
+	if strings.Contains(remoteURL, "git@github.com:") {
+		parts := strings.Split(strings.TrimPrefix(remoteURL, "git@github.com:"), "/")
+		owner = parts[0]
+		repoName = strings.TrimSuffix(parts[1], ".git")
+	} else {
+		parts := strings.Split(strings.TrimPrefix(remoteURL, "https://github.com/"), "/")
+		owner = parts[0]
+		repoName = strings.TrimSuffix(parts[1], ".git")
+	}
+
+	if githubToken == "" {
+		return fmt.Errorf("GitHub token not provided in settings")
+	}
+
+	newPR := &github.NewPullRequest{
+		Title:               github.String(input.Title),
+		Head:                github.String(input.Branch),
+		Base:                github.String(input.Base),
+		Body:                github.String(input.Description),
+		Draft:               github.Bool(input.Draft),
+		MaintainerCanModify: github.Bool(input.MaintainerCanModify),
+	}
+
+	pr, _, err := client.PullRequests.Create(ctx, owner, repoName, newPR)
+	if err != nil {
+		return fmt.Errorf("error creating PR: %v", err)
+	}
+
+    _, _, err = client.Issues.AddLabelsToIssue(ctx, owner, repoName, pr.GetNumber(), labels)
+	if err != nil {
+        return fmt.Errorf("error adding labels to PR: %v", err)
+    }
+
+	prLink := pr.GetHTMLURL()
+	log.Printf("PR created successfully: %s", prLink)
+
+	return nil
+}
+
+
 func FetchRepositories(githubToken string) ([]Repository, error) {
 	if githubToken == "" {
 		return nil, fmt.Errorf("GitHub token not provided in settings")
