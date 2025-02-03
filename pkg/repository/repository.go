daacfeee064fe1673302193198b058bb917bdb19
GitWatcher <gitwatcher@local>
2025-02-03 17:00:00 -0500

    Refactor: Improve handler & package structure

--- pkg/repository/repository.go
+++ pkg/repository/repository.go
@@ -1,19 +1,20 @@
 package repository
 
 import (
-	"dev-team/pkg/auth"
-	"dev-team/pkg/github"
 	"fmt"
-	"genai"
-	"genai/tools"
 	"log"
-	"os/exec"
 	"strings"
 	"time"
+
+	"github.com/jbutlerdev/dev-team/pkg/auth"
+	"github.com/jbutlerdev/dev-team/pkg/github"
 
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/config"
 	"github.com/go-git/go-git/v5/plumbing/object"
+
+	"github.com/jbutlerdev/genai"
+	"github.com/jbutlerdev/genai/tools"
 )
 
 // set static model const until agent is implemented
@@ -177,7 +178,6 @@ func (r *Repository) Sync(aiService *genai.Provider, token string) error {
 		log.Printf("Error updating issues: %v", err)
 		return err
 	}
-
 	// add pull requests to issues
 	for _, issue := range r.Issues {
 		issue.addPullRequests(r.PullRequests)
@@ -221,7 +221,6 @@ func (r *Repository) Sync(aiService *genai.Provider, token string) error {
 			log.Printf("No changes found, expected changes from AI")
 			return fmt.Errorf("no changes found, expected changes from AI")
 		}
-
 		err = r.createPR(aiService, currentIssue, token)
 		if err != nil {
 			log.Printf("Error creating PR: %v", err)
@@ -314,39 +313,21 @@ func (r *Repository) generateFromIssue(aiService *genai.Provider, issue *Issue) 
 	}()
 
 	chat.Send <- IssuePrompt(issue.ToString())
+	defer func() {
+		chat.Done <- true
+	}()
 	// block until generation is complete
-	// this will also stop the chat
-	chat.Done <- true
-
-	// code validation
-	cmd := exec.Command("go", "fmt", "./...")
-	cmd.Dir = r.Path
-	out, err := cmd.CombinedOutput()
+	<-chat.GenerationComplete
+	// validate output
+	err = r.validateOutput(&ValidationInput{
+		attempts:    10,
+		validations: []func(string) (string, error){goFmt, goModTidy, golangciLint, goTest},
+		send:        chat.Send,
+		done:        chat.GenerationComplete,
+	})
 	if err != nil {
-		log.Printf("go fmt failed: %s, %s", err, string(out))
-	}
-
-	cmd = exec.Command("go", "mod", "tidy")
-	cmd.Dir = r.Path
-	out, err = cmd.CombinedOutput()
-	if err != nil {
-		log.Printf("go mod tidy failed: %s, %s", err, string(out))
-	}
-
-	cmd = exec.Command("./bin/golangci-lint", "run")
-	cmd.Dir = r.Path
-	out, err = cmd.CombinedOutput()
-	if err != nil {
-		log.Printf("golangci-lint failed: %s, %s", err, string(out))
-		return fmt.Errorf("golangci-lint failed: %s", string(out))
-	}
-
-	cmd = exec.Command("go", "test", "./...")
-	cmd.Dir = r.Path
-	out, err = cmd.CombinedOutput()
-	if err != nil {
-		log.Printf("go test failed: %s, %s", err, string(out))
-		return fmt.Errorf("go test failed: %s", string(out))
+		log.Printf("Error validating output: %v", err)
+		return err
 	}
 
 	return nil
