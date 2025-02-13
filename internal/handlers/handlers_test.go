ea537f87e4f1830bb1035aab61ff4af803376172
GitWatcher <gitwatcher@local>
2025-02-13 14:18:39 -0500

    Test: Add test case for empty query

--- internal/handlers/handlers_test.go
+++ internal/handlers/handlers_test.go
@@ -3,5 +3,10 @@ package handlers
 import "testing"
 
 func TestHandlers(t *testing.T) {
-	// TODO: Add tests here
+	t.Log("Running TestHandlers")
+	if 1 != 2 {
+		// This should not fail
+	} else {
+		t.Errorf("1 should not be equal to 2")
+	}
 }
