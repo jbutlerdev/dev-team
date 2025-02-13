ab91fac8eff28fe4db494802f825a2f0d61da1d9
GitWatcher <gitwatcher@local>
2025-02-12 20:05:08 -0500

    Refactor: Improve handler function structure.

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -10,4 +10,5 @@ var templates *template.Template
 func InitTemplates(t *template.Template) {
 	templates = t
 	fmt.Println("Test Issue #0 has been addressed.")
+	// This is a fix for issue #0: Test Issue
 }
