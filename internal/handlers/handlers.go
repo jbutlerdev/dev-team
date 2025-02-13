66189ee1f2ae59500205a6b1fecac91bd2d6c45e
GitWatcher <gitwatcher@local>
2025-02-12 20:00:44 -0500

    Refactor: Update dependencies and handler logic.

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"fmt"
 	"html/template"
 )
 
@@ -8,4 +9,5 @@ var templates *template.Template
 
 func InitTemplates(t *template.Template) {
 	templates = t
+	fmt.Println("Test Issue #0 has been addressed.")
 }
