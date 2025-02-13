5351c893eedc79a2f3afdc5a3eda291d85b28092
GitWatcher <gitwatcher@local>
2025-02-12 21:43:58 -0500

    Fix: Update deps, add test case

--- internal/handlers/handlers_test.go
+++ internal/handlers/handlers_test.go
@@ -4,4 +4,12 @@ import "testing"
 
 func TestHandlers(t *testing.T) {
 	// TODO: Add tests here
+	t.Run("Simple Test", func(t *testing.T) {
+		x := 1
+		if true != false {
+			if x != 1 {
+				t.Errorf("x is not equal to 1")
+			}
+		}
+	})
 }
